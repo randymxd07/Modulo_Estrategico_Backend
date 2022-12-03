601d03213ff6fd3cbde3e5603af5ed066dacd427
randymxd06 <randym0624@gmail.com>
2022-12-03 19:44:11 -0400

    Quite la ruta de /public/public/productImages para que se suban las imagenes y tenerlas de prueba por si tengo que borrarlas y crearlas de nuevo mediante un seeder

--- app/Http/Controllers/OrderVsProductController.php
+++ app/Http/Controllers/OrderVsProductController.php
@@ -9,10 +9,7 @@ use Illuminate\Http\Request;
 
 class OrderVsProductController extends Controller
 {
-    /**
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function index(Request $request)
     {
         $orderVsProducts = OrderVsProduct::all();
@@ -20,19 +17,11 @@ class OrderVsProductController extends Controller
         return view('orderVsProduct.index', compact('orderVsProducts'));
     }
 
-    /**
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
     public function create(Request $request)
     {
         return view('orderVsProduct.create');
     }
 
-    /**
-     * @param \App\Http\Requests\OrderVsProductStoreRequest $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(OrderVsProductStoreRequest $request)
     {
         $orderVsProduct = OrderVsProduct::create($request->validated());
@@ -42,31 +31,16 @@ class OrderVsProductController extends Controller
         return redirect()->route('orderVsProduct.index');
     }
 
-    /**
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Models\OrderVsProduct $orderVsProduct
-     * @return \Illuminate\Http\Response
-     */
     public function show(Request $request, OrderVsProduct $orderVsProduct)
     {
         return view('orderVsProduct.show', compact('orderVsProduct'));
     }
 
-    /**
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Models\OrderVsProduct $orderVsProduct
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Request $request, OrderVsProduct $orderVsProduct)
     {
         return view('orderVsProduct.edit', compact('orderVsProduct'));
     }
 
-    /**
-     * @param \App\Http\Requests\OrderVsProductUpdateRequest $request
-     * @param \App\Models\OrderVsProduct $orderVsProduct
-     * @return \Illuminate\Http\Response
-     */
     public function update(OrderVsProductUpdateRequest $request, OrderVsProduct $orderVsProduct)
     {
         $orderVsProduct->update($request->validated());
@@ -76,15 +50,11 @@ class OrderVsProductController extends Controller
         return redirect()->route('orderVsProduct.index');
     }
 
-    /**
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Models\OrderVsProduct $orderVsProduct
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Request $request, OrderVsProduct $orderVsProduct)
     {
         $orderVsProduct->delete();
 
         return redirect()->route('orderVsProduct.index');
     }
+
 }
