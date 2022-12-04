14a97f074ce0ed27974b1928e7effe3d16afffb8
randymxd06 <randym0624@gmail.com>
2022-12-04 17:51:11 -0400

    Estoy devolviendo el precio de los productos en las ordenes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -25,7 +25,8 @@ class OrderController extends Controller
                     ->select(
                         'products.id as product_id',
                         'products.name as product_name',
-                        'order_vs_products.quantity'
+                        'order_vs_products.quantity',
+                        'products.price as product_price'
                     )
                     ->where('order_id', '=', $order->id)
                     ->get();
