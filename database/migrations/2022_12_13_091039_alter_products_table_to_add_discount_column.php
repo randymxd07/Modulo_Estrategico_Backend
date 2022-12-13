9db6ba5acac731d6be27875f42bcc6a4ffd554b0
randymxd06 <randym0624@gmail.com>
2022-12-13 09:16:15 -0400

    Agregue una columna en la tabla productos para los descuentos

---
+++ database/migrations/2022_12_13_091039_alter_products_table_to_add_discount_column.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->decimal('discount')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('discount');
+        });
+    }
+
+};
